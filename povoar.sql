639177cced57e3778d8c3937d86c1f71f5c62f4c
xRuiAlves <up201606746@fe.up.pt>
2018-04-12 23:02:16 +0100

    Added first version of 'povoar' script file

---
+++ povoar.sql
@@ -0,0 +1,28 @@
+-- Users
+INSERT INTO User VALUES (2034390363479219, 'reeckset', 0);
+INSERT INTO User VALUES (2391887954170140, 'ruizadas', 0);
+INSERT INTO User VALUES (1172259692795609, 'el_pistaccio', 0);
+INSERT INTO User VALUES (1213126072166765, 'amy_pond', 0);
+INSERT INTO User VALUES (1399959692665501, 'eXcolhe', 0);
+INSERT INTO User VALUES (1000000000000001, 'random123', 0);
+INSERT INTO User VALUES (1000000000000002, 'helloWorld', 0);
+INSERT INTO User VALUES (1000000000000003, 'dataBasesAreCool', 0);
+INSERT INTO User VALUES (1000000000000004, 'SQLemPapel', 0);
+INSERT INTO User VALUES (1000000000000005, 'sim_nao_talvez', 0);
+
+-- Exercise Types
+INSERT INTO ExerciseType VALUES (0, 'Endurance');
+INSERT INTO ExerciseType VALUES (1, 'Strength');
+INSERT INTO ExerciseType VALUES (2, 'Balance');
+INSERT INTO ExerciseType VALUES (3, 'Flexibity');
+
+-- Week Days
+INSERT INTO WeekDay VALUES (0, 'Sunday');
+INSERT INTO WeekDay VALUES (1, 'Monday');
+INSERT INTO WeekDay VALUES (2, 'Tuesday');
+INSERT INTO WeekDay VALUES (3, 'Wednesday');
+INSERT INTO WeekDay VALUES (4, 'Thursday');
+INSERT INTO WeekDay VALUES (5, 'Friday');
+INSERT INTO WeekDay VALUES (6, 'Saturday');
+
+
